1c279d0c148c81a363ea1056972c575cdf08020d
M. Arkan Nibrastama <59256068+ArkanNibrastama@users.noreply.github.com>
2023-07-06 23:32:04 +0700

    Delete data_lake directory

--- data_lake/main.tf
+++
@@ -1,9 +0,0 @@
-# DATA LAKE
-resource "google_storage_bucket" "data_lake"{
-
-    name = var.name
-    location = var.region
-    storage_class = var.storage_class
-    uniform_bucket_level_access = var.bucket_access
-
-}
